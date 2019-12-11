dea163bab9184ab29ced623b08d2a88bacad9209
jleightcap <jleightcap@gmail.com>
2019-12-11 05:12:04 -0500

    Add external frame count to video.sh
    Now takes in external .txt file containing frame information.
    Parses array of times, inputs string appropriately into eval call.

--- video.sh
+++ video.sh
@@ -6,20 +6,41 @@
 FPS=60
 IMGS="./Sheets"
 OUT="test.mp4"
+FRAMES_DATA="frames.txt"
+
 
 
 #############
 # SCRIPTING #
 #############
 FILES=`find $IMGS/*.png`
-# import images directory
+
+# Manually count images in $IMGS
+let IMG_COUNT=0
+for _ in ${FILES[@]}; do (( IMG_COUNT+=1 )); done
+
+# Start $FULL_SCRIPT, the eventual string to be eval'd.
+# Import images directory
 FULL_SCRIPT="ffmpeg -i $IMGS/%3d.png "
-# import audio, argv[1]
+# Import audio, argv[1]
 FULL_SCRIPT+="-i $1 "
-# timing
-FULL_SCRIPT+="-vf \"zoompan=d=25\" "
-#+'50*eq(in,1)\" "
+
+# Read frame data 
+declare -a FRAMES
+while read line; do
+  [[ "$line" =~ ^[[:space:]]*# ]] && continue
+  FRAMES+=($line)
+done < $FRAMES_DATA
+
+FRAME_COEF=""
+for (( ii=0; ii<$IMG_COUNT-1; ii++ )) do
+  FRAME_COEF+="'${FRAMES[$ii]}*eq(in,$ii)'+";
+done
+FRAME_COEF+="'50*eq(in,$IMG_COUNT)'"
+
+FULL_SCRIPT+="-vf \"zoompan=d=$FRAME_COEF\" "
 FULL_SCRIPT+=$OUT
 
 echo $FULL_SCRIPT
 eval ${FULL_SCRIPT}
+unset -v FRAMES
